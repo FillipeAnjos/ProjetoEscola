ebe0fd5b3fb8fc641dfdbe7cf128e42f5ad7e9a2
FillipeAnjos <fillipe.anjos.2000@hotmail.com>
2021-11-24 18:12:15 -0300

    Criação para o Context

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 //import './styles/global.scss';
 import Index from "./components/index";
 import { GlobalStyle } from './styles/global';
+import { TransactionsContext, TransactionsProvider } from './TransactionsContext';
 
 const App = () => {
   return(
-    <>
+    <TransactionsProvider>
       <GlobalStyle />    
       <Index />
-    </>
+    </TransactionsProvider>
   )
 }
 
--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,11 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useState, useContext } from 'react';
 import Modal from 'react-modal';
 import closeImg from '../../assets/close.png';
 import trueImg from '../../assets/true.png';
 import falseImg from '../../assets/false.png';
 import { Container, TransactionTypeContainer, RadioBox } from '../NewTransactionModal/style';
 import { api } from '../../services/api';
+import { TransactionsContext } from '../../TransactionsContext';
 
 interface NewTransacionModalProps {
     isOpen: boolean;
@@ -12,22 +13,24 @@ interface NewTransacionModalProps {
 }
 
 export function NewTransacionModal({ isOpen, onRequestClose }: NewTransacionModalProps){
+    const { createTransaction } = useContext(TransactionsContext);
+
+
+
     const [title, setTitle] = useState('');
-    const [value, setValue] = useState(0);
+    const [amount, setAmount] = useState(0);
     const [category, setCategory] = useState('');
     const [type, setType] = useState('deposit');
 
     function handleCreateNewTransaction(event: FormEvent){
         event.preventDefault();//Previnir o funcionamento padrão de carregar a página automático ao clicar no submit no html
 
-        const data = {
+        createTransaction({
             title,
-            value,
+            amount,
             category,
-            type
-        };
-
-        api.post('/transactions', data)
+            type,
+        })
     }
 
     return (
@@ -57,8 +60,8 @@ export function NewTransacionModal({ isOpen, onRequestClose }: NewTransacionModa
                 <input 
                     type="number" 
                     placeholder="Valor" 
-                    value={value} 
-                    onChange={ event => setValue(Number(event.target.value)) }
+                    value={amount} 
+                    onChange={ event => setAmount(Number(event.target.value)) }
                 />
 
                 <TransactionTypeContainer>
